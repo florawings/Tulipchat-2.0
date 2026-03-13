15c481fa138b175af9647131b18628e7c30854a3
florawings <epffo.portal@gmail.com>
2026-03-14 02:05:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,61 +1,45 @@
 const express = require("express")
-const http = require("http")
-const {Server} = require("socket.io")
-
 const app = express()
-const server = http.createServer(app)
-const io = new Server(server)
+const http = require("http").createServer(app)
+const io = require("socket.io")(http)
 
-app.use(express.json())
 app.use(express.static("public"))
 
 let users = []
-
-app.post("/register",(req,res)=>{
-
-const {username,email,password,dob,gender,country} = req.body
-
-if(!username || !email || !password){
-return res.json({ok:false})
-}
-
-users.push({
-username,email,password,dob,gender,country
-})
-
-res.json({ok:true})
-
-})
-
-app.post("/login",(req,res)=>{
-
-const {username,password} = req.body
-
-let user = users.find(u=>u.username===username && u.password===password)
-
-if(user){
-res.json({ok:true})
-}else{
-res.json({ok:false})
-}
-
-})
 
 io.on("connection",(socket)=>{
 
 socket.on("join",(user)=>{
 
-socket.user=user
-io.emit("msg","SYSTEM : "+user+" joined")
+socket.username = user
+users.push(user)
+
+io.emit("system","SYSTEM : " + user + " joined")
+
+io.emit("users",users)
 
 })
 
 socket.on("chat",(data)=>{
 
-io.emit("msg",data.user+" : "+data.text)
+let msg = data.user + " : " + data.text
+
+io.emit("msg",msg)
+
+})
+
+socket.on("disconnect",()=>{
+
+users = users.filter(u => u !== socket.username)
+
+io.emit("users",users)
 
 })
 
 })
 
-server.listen(3000)
+http.listen(3000,()=>{
+
+console.log("Server running")
+
+})
