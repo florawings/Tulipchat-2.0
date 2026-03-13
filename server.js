c0466d7be15cfb1328aa35fba623d90aa12f3de1
florawings <epffo.portal@gmail.com>
2026-03-14 00:13:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,26 +10,38 @@ let users = {}
 
 io.on("connection",(socket)=>{
 
-socket.on("join",(username)=>{
-users[username] = socket.id
+socket.on("join",(data)=>{
+
+socket.join(data.room)
+
+users[data.username] = socket.id
+
 io.emit("users",Object.keys(users))
+
 })
 
 socket.on("chat message",(data)=>{
-io.emit("chat message",data)
+
+io.to(data.room).emit("chat message",data)
+
 })
 
 socket.on("disconnect",()=>{
+
 for(let name in users){
-if(users[name] == socket.id){
+
+if(users[name]==socket.id){
+
 delete users[name]
+
 }
+
 }
+
 io.emit("users",Object.keys(users))
-})
 
 })
 
-http.listen(process.env.PORT || 3000,()=>{
-console.log("Server running")
 })
+
+http.listen(process.env.PORT || 3000)
