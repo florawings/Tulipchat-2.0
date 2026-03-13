ac4a89f7f76f9bfb120f63569146f3fc4f169228
florawings <epffo.portal@gmail.com>
2026-03-14 01:42:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require("express")
+const http = require("http")
+const {Server} = require("socket.io")
+
 const app = express()
+const server = http.createServer(app)
+const io = new Server(server)
 
 app.use(express.json())
+app.use(express.static("public"))
 
 let users = []
 
@@ -9,17 +15,12 @@ app.post("/register",(req,res)=>{
 
 const {username,email,password,dob,gender,country} = req.body
 
-if(!email || !password){
+if(!username || !email || !password){
 return res.json({ok:false})
 }
 
 users.push({
-username,
-email,
-password,
-dob,
-gender,
-country
+username,email,password,dob,gender,country
 })
 
 res.json({ok:true})
@@ -30,7 +31,7 @@ app.post("/login",(req,res)=>{
 
 const {username,password} = req.body
 
-let user = users.find(u=>u.username==username && u.password==password)
+let user = users.find(u=>u.username===username && u.password===password)
 
 if(user){
 res.json({ok:true})
@@ -40,4 +41,21 @@ res.json({ok:false})
 
 })
 
-app.listen(3000)
+io.on("connection",(socket)=>{
+
+socket.on("join",(user)=>{
+
+socket.user=user
+io.emit("msg","SYSTEM : "+user+" joined")
+
+})
+
+socket.on("chat",(data)=>{
+
+io.emit("msg",data.user+" : "+data.text)
+
+})
+
+})
+
+server.listen(3000)
