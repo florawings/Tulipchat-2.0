48ad82eddfdc29895ee716eb036973e5dd464aab
florawings <epffo.portal@gmail.com>
2026-03-13 23:45:36 +0530

    Refactor socket event handling in server.js

--- server.js
+++ server.js
@@ -1,23 +1,36 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+app.use(express.static("public"));
+
 const onlineUsers = {};
 
 io.on("connection", (socket) => {
 
-socket.on("join", (username)=>{
+console.log("User connected");
+
+socket.on("join", (username) => {
 onlineUsers[socket.id] = username;
 io.emit("users", Object.values(onlineUsers));
 });
 
-socket.on("chat message",(data)=>{
-io.emit("chat message",data);
+socket.on("chat message", (data) => {
+io.emit("chat message", data);
 });
 
-socket.on("typing",(username)=>{
-socket.broadcast.emit("typing",username);
+socket.on("typing", (username) => {
+socket.broadcast.emit("typing", username);
 });
 
-socket.on("disconnect",()=>{
+socket.on("disconnect", () => {
 delete onlineUsers[socket.id];
 io.emit("users", Object.values(onlineUsers));
 });
 
+});
+
+http.listen(process.env.PORT || 3000, () => {
+console.log("Server running");
 });
