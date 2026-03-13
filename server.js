e9a8f98d9fb36a92f3dbaa4c1ec6b2c9da6385d3
florawings <epffo.portal@gmail.com>
2026-03-14 01:10:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,47 @@
 const express = require("express")
+const fs = require("fs")
 const app = express()
 
 const http = require("http").createServer(app)
 const io = require("socket.io")(http)
 
+app.use(express.json())
 app.use(express.static("public"))
 
 let users = {}
 
+if (fs.existsSync("users.json")) {
+users = JSON.parse(fs.readFileSync("users.json"))
+}
+
+app.post("/register",(req,res)=>{
+const {email,password} = req.body
+
+if(users[email]){
+return res.json({status:"exists"})
+}
+
+users[email] = password
+
+fs.writeFileSync("users.json",JSON.stringify(users))
+
+res.json({status:"ok"})
+})
+
+app.post("/login",(req,res)=>{
+const {email,password} = req.body
+
+if(users[email] && users[email] === password){
+return res.json({status:"ok"})
+}
+
+res.json({status:"error"})
+})
+
 io.on("connection",(socket)=>{
 
-console.log("User connected")
-
-socket.on("chat message",(data)=>{
-
-io.emit("chat message",data)
-
+socket.on("chat message",(msg)=>{
+io.emit("chat message",msg)
 })
 
 })
@@ -23,5 +49,5 @@ io.emit("chat message",data)
 const PORT = process.env.PORT || 3000
 
 http.listen(PORT,()=>{
-console.log("Server running on",PORT)
+console.log("Server running on "+PORT)
 })
