cae9a0ad6650a374c22ca727b8c01a5bf80b1c9a
florawings <epffo.portal@gmail.com>
2026-03-13 23:44:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,37 +1,23 @@
-const express = require("express");
-const app = express();
-const http = require("http").createServer(app);
-const io = require("socket.io")(http);
-
-app.use(express.json());
-app.use(express.static("public"));
-
-const users = {}; // { username: password }
-
-// register
-app.post("/api/register", (req, res) => {
-  const { username, password } = req.body;
-  if (!username || !password) return res.json({ ok: false, msg: "Missing fields" });
-  if (users[username]) return res.json({ ok: false, msg: "User exists" });
-
-  users[username] = password;
-  res.json({ ok: true });
-});
-
-// login
-app.post("/api/login", (req, res) => {
-  const { username, password } = req.body;
-  if (users[username] && users[username] === password) {
-    return res.json({ ok: true });
-  }
-  res.json({ ok: false, msg: "Invalid login" });
-});
+const onlineUsers = {};
 
 io.on("connection", (socket) => {
-  socket.on("chat message", (data) => {
-    io.emit("chat message", data);
-  });
+
+socket.on("join", (username)=>{
+onlineUsers[socket.id] = username;
+io.emit("users", Object.values(onlineUsers));
 });
 
-const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => console.log("Server running on " + PORT));
+socket.on("chat message",(data)=>{
+io.emit("chat message",data);
+});
+
+socket.on("typing",(username)=>{
+socket.broadcast.emit("typing",username);
+});
+
+socket.on("disconnect",()=>{
+delete onlineUsers[socket.id];
+io.emit("users", Object.values(onlineUsers));
+});
+
+});
