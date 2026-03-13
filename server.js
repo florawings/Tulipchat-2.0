9bcc635b966dec31d9bb83b0c6ba999bfbca240f
florawings <epffo.portal@gmail.com>
2026-03-13 23:55:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,50 +10,22 @@ let users = {}
 
 io.on("connection",(socket)=>{
 
-console.log("User connected")
-
 socket.on("join",(username)=>{
-
 users[username] = socket.id
-
 io.emit("users",Object.keys(users))
-
 })
 
 socket.on("chat message",(data)=>{
-
 io.emit("chat message",data)
-
-})
-
-socket.on("private message",(data)=>{
-
-let target = users[data.to]
-
-if(target){
-io.to(target).emit("private message",data)
-}
-
-})
-
-socket.on("typing",(username)=>{
-
-socket.broadcast.emit("typing",username)
-
 })
 
 socket.on("disconnect",()=>{
-
 for(let name in users){
 if(users[name] == socket.id){
 delete users[name]
 }
 }
-
 io.emit("users",Object.keys(users))
-
-console.log("User disconnected")
-
 })
 
 })
