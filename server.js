5ae430f968f0f416a7ac3ce17911f735575a1f84
florawings <epffo.portal@gmail.com>
2026-03-14 13:53:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,67 +1,43 @@
 const express = require("express")
-const bodyParser = require("body-parser")
-
 const app = express()
+const http = require("http").createServer(app)
+const io = require("socket.io")(http)
 
-app.use(bodyParser.json())
 app.use(express.static("public"))
 
-let users = []
+let users = {}
 
+io.on("connection",(socket)=>{
 
-// REGISTER
-app.post("/register",(req,res)=>{
+socket.on("join",(data)=>{
 
-const {username,email,password,dob,country} = req.body
-
-const birth = new Date(dob)
-const age = new Date().getFullYear() - birth.getFullYear()
-
-if(age < 18){
-return res.json({success:false,message:"Only 18+ allowed"})
+users[socket.id] = {
+name:data.name,
+type:data.type
 }
-
-const exist = users.find(u=>u.username===username)
-
-if(exist){
-return res.json({success:false,message:"Username already exists"})
-}
-
-users.push({
-username,
-email,
-password,
-dob,
-country
-})
-
-res.json({success:true})
 
 })
 
+socket.on("message",(msg)=>{
 
+let user = users[socket.id]
 
-// LOGIN
-app.post("/login",(req,res)=>{
+if(!user) return
 
-const {username,password} = req.body
-
-const user = users.find(
-u=>u.username===username && u.password===password
-)
-
-if(!user){
-return res.json({success:false})
-}
-
-res.json({success:true})
+io.emit("message",{
+user:user.name,
+type:user.type,
+text:msg
+})
 
 })
 
+socket.on("disconnect",()=>{
+delete users[socket.id]
+})
 
+})
 
-app.listen(3000,()=>{
-
-console.log("Tulip Chat server running")
-
+http.listen(3000,()=>{
+console.log("Server running")
 })
