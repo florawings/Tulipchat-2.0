8beb79f906913a978d4f73219d35821651aca351
florawings <epffo.portal@gmail.com>
2026-03-14 15:37:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,95 +3,28 @@ const app = express()
 const http = require("http").createServer(app)
 const io = require("socket.io")(http)
 
-app.use(express.json())
 app.use(express.static("public"))
 
-const OWNER = {
-username:"Lord_lucifer",
-password:"766521",
-role:"owner"
-}
-
-let users=[]
-let rooms=["public"]
-let onlineUsers={}
-
-/* REGISTER */
-
-app.post("/register",(req,res)=>{
-
-let {username,password}=req.body
-
-if(username===OWNER.username){
-return res.json({status:"reserved"})
-}
-
-let exist=users.find(u=>u.username===username)
-
-if(exist){
-return res.json({status:"exists"})
-}
-
-users.push({
-username,
-password,
-role:"user"
-})
-
-res.json({status:"registered"})
-
-})
-
-/* LOGIN */
-
-app.post("/login",(req,res)=>{
-
-let {username,password}=req.body
-
-if(username===OWNER.username && password===OWNER.password){
-
-return res.json({
-status:"owner",
-username:OWNER.username
-})
-
-}
-
-let user=users.find(
-u=>u.username===username && u.password===password
-)
-
-if(!user){
-return res.json({status:"invalid"})
-}
-
-res.json({
-status:"success",
-username:user.username,
-role:user.role
-})
-
-})
-
-/* SOCKET */
+let users = []
 
 io.on("connection",(socket)=>{
 
-socket.on("join",(data)=>{
+socket.on("join",(name)=>{
+users.push(name)
+io.emit("users",users)
+})
 
-onlineUsers[socket.id]={
-name:data.name
-}
+socket.on("message",(data)=>{
+io.emit("message",data)
+})
 
-socket.join("public")
-
-io.emit("system",data.name+" joined")
+socket.on("disconnect",()=>{
+users = users.filter(u=>u!==socket.username)
+io.emit("users",users)
+})
 
 })
 
-/* MESSAGE */
-
-socket.on("message",(data)=>{
-
-io.to(data.room).emit("message",{
-user:data.user,
+http.listen(process.env.PORT || 3000,()=>{
+console.log("Tulip Chat server running")
+})
