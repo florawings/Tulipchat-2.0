920f43495fa94554ee34efb9adee71473d5c5b2a
florawings <epffo.portal@gmail.com>
2026-03-15 01:10:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const express = require("express")
 const app = express()
+
 const http = require("http").createServer(app)
 const io = require("socket.io")(http)
+
 const path = require("path")
 
 app.use(express.static(path.join(__dirname,"public")))
-
-app.get("/",(req,res)=>{
-res.sendFile(path.join(__dirname,"public","index.html"))
-})
 
 let users = {}
 
@@ -18,35 +16,41 @@ socket.on("join",(name)=>{
 
 users[socket.id] = name
 
-io.emit("system", name + " joined the chat")
+io.emit("chat",{
+type:"join",
+text:name+" joined the chat"
+})
 
-io.emit("users", users)
+io.emit("users",users)
 
 })
 
 socket.on("message",(data)=>{
 
-io.emit("message", data)
+io.emit("chat",{
+type:"text",
+name:data.name,
+text:data.text
+})
 
 })
 
 socket.on("image",(data)=>{
 
-io.emit("image", data)
+io.emit("chat",{
+type:"image",
+name:data.name,
+url:data.url
+})
 
 })
 
 socket.on("gif",(data)=>{
 
-io.emit("gif", data)
-
-})
-
-socket.on("dm",(data)=>{
-
-io.to(data.to).emit("dm",{
-from:data.name,
-text:data.text
+io.emit("chat",{
+type:"gif",
+name:data.name,
+url:data.url
 })
 
 })
@@ -55,11 +59,18 @@ socket.on("disconnect",()=>{
 
 let name = users[socket.id]
 
+if(name){
+
+io.emit("chat",{
+type:"left",
+text:name+" left the chat"
+})
+
 delete users[socket.id]
 
-io.emit("system", name + " left the chat")
+io.emit("users",users)
 
-io.emit("users", users)
+}
 
 })
 
@@ -68,7 +79,5 @@ io.emit("users", users)
 const PORT = process.env.PORT || 10000
 
 http.listen(PORT,()=>{
-
 console.log("server running")
-
 })
