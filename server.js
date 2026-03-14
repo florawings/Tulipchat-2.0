a0ed521ef39f498ba198f05a26426095ba3f153e
florawings <epffo.portal@gmail.com>
2026-03-14 23:11:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,143 +1,103 @@
-const express = require("express")
-const http = require("http")
-const { Server } = require("socket.io")
-const { MongoClient } = require("mongodb")
+const express=require("express")
+const http=require("http")
+const {Server}=require("socket.io")
+const {MongoClient}=require("mongodb")
 
-const app = express()
-const server = http.createServer(app)
-const io = new Server(server)
+const app=express()
+const server=http.createServer(app)
+const io=new Server(server)
 
 app.use(express.static("public"))
-app.use(express.json())
 
-const PORT = process.env.PORT || 3000
+const PORT=process.env.PORT||3000
 
-/* ---------------- MongoDB ---------------- */
+/* MongoDB */
 
-const uri =
-"mongodb+srv://epffoportal_db_user:wAaE19Wqq3XFMbJH@cluster0.mighbsf.mongodb.net/?retryWrites=true&w=majority"
+const uri="YOUR_MONGODB_URL"
 
-const client = new MongoClient(uri)
+const client=new MongoClient(uri)
 
 let messages
 
-async function connectDB() {
+async function start(){
 
 await client.connect()
 
-const db = client.db("tulipchat")
+const db=client.db("tulipchat")
 
-messages = db.collection("messages")
-
-/* auto delete messages after 2 hours */
+messages=db.collection("messages")
 
 await messages.createIndex(
-{ createdAt: 1 },
-{ expireAfterSeconds: 7200 }
+{createdAt:1},
+{expireAfterSeconds:7200}
 )
 
-console.log("MongoDB connected")
+console.log("Mongo connected")
 
 }
 
-connectDB()
+start()
 
-/* ---------------- USERS ---------------- */
+/* USERS */
 
-let users = {}
+let users={}
 
-/* ---------------- SOCKET ---------------- */
+/* SOCKET */
 
-io.on("connection", (socket) => {
+io.on("connection",(socket)=>{
 
-console.log("User connected")
+socket.on("join",(username)=>{
 
-/* JOIN ROOM */
+users[socket.id]=username
 
-socket.on("joinRoom", async (data) => {
+io.emit("online",Object.values(users))
 
-const { username, room } = data
-
-socket.join(room)
-
-users[socket.id] = { username, room }
-
-io.to(room).emit("system", username + " joined the room")
-
-/* send last messages */
-
-const old = await messages
-.find({ room })
-.sort({ createdAt: -1 })
-.limit(20)
-.toArray()
-
-socket.emit("oldMessages", old.reverse())
+io.emit("system",username+" joined")
 
 })
 
-/* MESSAGE */
+socket.on("message",async(data)=>{
 
-socket.on("chatMessage", async (data) => {
-
-const msg = {
-username: data.username,
-room: data.room,
-message: data.message,
-createdAt: new Date()
+const msg={
+user:data.user,
+text:data.text,
+createdAt:new Date()
 }
 
 await messages.insertOne(msg)
 
-io.to(data.room).emit("message", msg)
+io.emit("message",msg)
 
 })
 
-/* OWNER ACTIONS */
+socket.on("image",(data)=>{
 
-socket.on("kickUser", (user) => {
-
-io.emit("system", user + " was kicked by owner")
+io.emit("image",data)
 
 })
 
-socket.on("banUser", (user) => {
+socket.on("dm",(data)=>{
 
-io.emit("system", user + " was banned by owner")
-
-})
-
-socket.on("muteUser", (user) => {
-
-io.emit("system", user + " was muted by owner")
+io.to(data.to).emit("dm",data)
 
 })
 
-/* DISCONNECT */
+socket.on("disconnect",()=>{
 
-socket.on("disconnect", () => {
-
-const user = users[socket.id]
-
-if (user) {
-
-io.to(user.room).emit(
-"system",
-user.username + " left the room"
-)
+let user=users[socket.id]
 
 delete users[socket.id]
 
-}
+io.emit("online",Object.values(users))
+
+if(user) io.emit("system",user+" left")
 
 })
 
 })
 
-/* ---------------- START SERVER ---------------- */
+server.listen(PORT,()=>{
 
-server.listen(PORT, () => {
-
-console.log("Server running on port " + PORT)
+console.log("server running")
 
 })
