b693768755e186cb6eb7f74f96a427c8c2859be7
florawings <epffo.portal@gmail.com>
2026-03-15 01:58:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
-const express = require("express");
-const app = express();
-const http = require("http").createServer(app);
-const { Server } = require("socket.io");
+const express = require("express")
+const app = express()
 
-const io = new Server(http);
+const http = require("http").createServer(app)
+const { Server } = require("socket.io")
 
-app.use(express.static("public"));
+const io = new Server(http)
 
-let users = {};
+app.use(express.static("public"))
+
+let users = {}
+let messages = []   // chat history
 
 io.on("connection",(socket)=>{
 
 socket.on("join",(data)=>{
 
 users[socket.id] = data
+
+socket.emit("history",messages)   // old messages send
 
 io.emit("system",data.name+" joined the chat")
 
@@ -23,17 +27,23 @@ io.emit("users",users)
 
 socket.on("message",(data)=>{
 
+messages.push({type:"text",data:data})
+
 io.emit("message",data)
 
 })
 
 socket.on("image",(data)=>{
 
+messages.push({type:"image",data:data})
+
 io.emit("image",data)
 
 })
 
 socket.on("gif",(data)=>{
+
+messages.push({type:"gif",data:data})
 
 io.emit("gif",data)
 
