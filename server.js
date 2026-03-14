dfa42c4e922984308e382d0fd04609d4edded9da
florawings <epffo.portal@gmail.com>
2026-03-14 22:33:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,30 +1,83 @@
 const express = require("express")
+const http = require("http")
+const { Server } = require("socket.io")
+const { MongoClient } = require("mongodb")
+
 const app = express()
-const http = require("http").createServer(app)
-const io = require("socket.io")(http)
+const server = http.createServer(app)
+const io = new Server(server)
 
 app.use(express.static("public"))
 
-let users = []
+/* MongoDB connection */
 
-io.on("connection",(socket)=>{
+const uri = "mongodb+srv://epffoportal_db_user:wAaE19Wqq3XFMbJH@cluster0.mighbsf.mongodb.net/?retryWrites=true&w=majority"
 
-socket.on("join",(name)=>{
-users.push(name)
-io.emit("users",users)
+const client = new MongoClient(uri)
+
+let messagesCollection
+
+async function startDB(){
+
+await client.connect()
+
+const db = client.db("tulipchat")
+
+messagesCollection = db.collection("messages")
+
+console.log("MongoDB connected")
+
+}
+
+startDB()
+
+/* Socket connection */
+
+io.on("connection", (socket)=>{
+
+console.log("User connected")
+
+socket.on("joinRoom", async(room)=>{
+
+socket.join(room)
+
+/* load old messages */
+
+const oldMessages = await messagesCollection
+.find({room:room})
+.sort({time:1})
+.limit(50)
+.toArray()
+
+socket.emit("oldMessages", oldMessages)
+
 })
 
-socket.on("message",(data)=>{
-io.emit("message",data)
+socket.on("chatMessage", async(data)=>{
+
+const message = {
+username:data.username,
+room:data.room,
+message:data.message,
+time:new Date()
+}
+
+await messagesCollection.insertOne(message)
+
+io.to(data.room).emit("message", message)
+
 })
 
 socket.on("disconnect",()=>{
-users = users.filter(u=>u!==socket.username)
-io.emit("users",users)
-})
+
+console.log("User disconnected")
 
 })
 
-http.listen(process.env.PORT || 3000,()=>{
-console.log("Tulip Chat server running")
+})
+
+server.listen(3000, ()=>{
+
+console.log("Server running")
+
 })
