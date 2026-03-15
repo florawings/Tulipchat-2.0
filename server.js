3536e46bf84ad5a22e95d5193c47f0aa2901920d
florawings <epffo.portal@gmail.com>
2026-03-16 02:09:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,113 +3,118 @@ const app = express()
 const http = require("http").createServer(app)
 const io = require("socket.io")(http)
 
-const bcrypt = require("bcryptjs")
-
-const User = require("./models/User")
-
-app.use(express.json())
 app.use(express.static("public"))
+app.use(express.json())
 
-let onlineUsers=[]
+let onlineUsers = []
+let mutedUsers = []
+let bannedUsers = []
 
-/* REGISTER */
-
-app.post("/register",async(req,res)=>{
-
-const {username,age,gender,email,password}=req.body
-
-let exist = await User.findOne({username})
-
-if(exist){
-
-return res.json({msg:"Username already exists"})
-
-}
-
-const hash = await bcrypt.hash(password,10)
-
-await User.create({
-
-username,
-age,
-gender,
-email,
-password:hash
-
-})
-
-res.json({msg:"registered"})
-
-})
-
-/* LOGIN */
-
-app.post("/login",async(req,res)=>{
-
-const {username,password}=req.body
-
-const user = await User.findOne({username})
-
-if(!user){
-
-return res.json({msg:"User not found"})
-}
-
-const ok = await bcrypt.compare(password,user.password)
-
-if(!ok){
-
-return res.json({msg:"Wrong password"})
-}
-
-res.json({
-
-msg:"login success",
-
-username:user.username,
-role:user.role,
-gender:user.gender
-
-})
-
-})
-
-/* SOCKET */
+const OWNER = "Lord_lucifer"
+const SUPER_ADMIN = "Garima"
 
 io.on("connection",(socket)=>{
 
 socket.on("join",(data)=>{
 
-socket.username=data.username
-socket.role=data.role
+socket.username = data.username
+
+socket.role = "user"
+
+if(data.username === OWNER){
+socket.role="owner"
+}
+
+if(data.username === SUPER_ADMIN){
+socket.role="superadmin"
+}
 
 onlineUsers.push({
-
 id:socket.id,
 username:data.username,
-role:data.role
-
+role:socket.role
 })
 
 io.emit("onlineUsers",onlineUsers)
 
+io.emit("system",data.username+" joined the chat")
+
 })
+
+/* MESSAGE */
 
 socket.on("message",(data)=>{
 
-if(data.msg==="/clear"){
-
-io.emit("clearChat")
+if(bannedUsers.includes(data.username)){
+socket.disconnect()
 return
 }
 
-io.emit("message",data)
+if(mutedUsers.includes(data.username)){
+socket.emit("system","You are muted")
+return
+}
+
+if(data.msg === "/clear"){
+
+if(socket.role==="owner" || socket.role==="superadmin"){
+io.emit("clearChat")
+}
+
+return
+}
+
+io.emit("message",{
+username:data.username,
+role:socket.role,
+msg:data.msg
+})
+
+})
+
+/* BAN */
+
+socket.on("banUser",(target)=>{
+
+if(socket.role!=="owner" && socket.role!=="superadmin") return
+
+bannedUsers.push(target)
+
+io.emit("system",target+" banned")
+
+})
+
+/* MUTE */
+
+socket.on("muteUser",(target)=>{
+
+if(socket.role!=="owner" && socket.role!=="superadmin") return
+
+mutedUsers.push(target)
+
+io.emit("system",target+" muted")
+
+})
+
+/* KICK */
+
+socket.on("kickUser",(target)=>{
+
+if(socket.role!=="owner" && socket.role!=="superadmin") return
+
+onlineUsers.forEach(u=>{
+
+if(u.username===target){
+io.to(u.id).disconnectSockets()
+}
+
+})
 
 })
 
 socket.on("disconnect",()=>{
 
-onlineUsers = onlineUsers.filter(u=>u.id!==socket.id)
+onlineUsers = onlineUsers.filter(u=>u.id !== socket.id)
 
 io.emit("onlineUsers",onlineUsers)
 
@@ -119,6 +124,6 @@ io.emit("onlineUsers",onlineUsers)
 
 http.listen(3000,()=>{
 
-console.log("Server running")
+console.log("Tulip Chat Server Running")
 
 })
