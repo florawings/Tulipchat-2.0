aa0658fbd7b754a16e445cac316548d36008845c
florawings <epffo.portal@gmail.com>
2026-03-15 21:55:50 +0530

    Update client.js

--- public/client.js
+++ public/client.js
@@ -1,102 +1,86 @@
-const socket = io();
+const socket = io()
 
-let username = localStorage.getItem("username");
-if (!username) {
-username = prompt("Enter username");
-localStorage.setItem("username", username);
+let room = "normal"
+let username = localStorage.getItem("user") || prompt("Enter username")
+
+socket.emit("user",username)
+
+function joinRoom(r){
+
+room = r
+
+socket.emit("join",room)
+
 }
 
-let room = "normal";
-let currentDM = null;
+function addMsg(html){
 
-socket.emit("join", { username, room });
+let div = document.createElement("div")
+div.className = "msg"
+div.innerHTML = html
 
-/* Send room message */
-function sendMessage() {
-let text = document.getElementById("text").value;
-if (!text) return;
+document.getElementById("messages").appendChild(div)
 
-socket.emit("message", { user: username, msg: text, room });
-document.getElementById("text").value = "";
+document.getElementById("messages").scrollTop =
+document.getElementById("messages").scrollHeight
+
 }
 
-/* Receive message */
-socket.on("message", (data) => {
-let box = document.getElementById("messages");
-box.innerHTML += "<div><b>${data.user}</b>: ${data.msg}</div>";
-box.scrollTop = box.scrollHeight;
-});
+function sendMsg(){
 
-/* System message */
-socket.on("system", (msg) => {
-document.getElementById("messages").innerHTML += "<div style="opacity:.6">${msg}</div>";
-});
+let text = document.getElementById("text").value
 
-/* Online users */
-socket.on("online", (list) => {
-let box = document.getElementById("online");
-box.innerHTML = "";
-list.forEach(u => {
-let d = document.createElement("div");
-d.innerText = u;
-d.onclick = () => openDM(u);
-box.appendChild(d);
-});
-});
+if(!text) return
 
-/* Open DM */
-function openDM(user) {
-currentDM = user;
-document.getElementById("dmBox").style.display = "block";
-document.getElementById("dmUser").innerText = "Chat with " + user;
+socket.emit("message",{
+room:room,
+user:username,
+msg:text
+})
+
+document.getElementById("text").value=""
+
 }
 
-/* Send DM */
-function sendDM() {
-let text = document.getElementById("dmText").value;
+socket.on("message",(data)=>{
 
-socket.emit("dm", {
-from: username,
-to: currentDM,
-msg: text
-});
+addMsg("<b>"+data.user+":</b> "+data.msg)
 
-document.getElementById("dmMessages").innerHTML += "<div><b>Me:</b> ${text}</div>";
-document.getElementById("dmText").value = "";
+})
+
+/* upload */
+
+document.getElementById("file").onchange = function(){
+
+let file = this.files[0]
+
+let form = new FormData()
+form.append("file",file)
+
+let xhr = new XMLHttpRequest()
+
+xhr.open("POST","/upload")
+
+xhr.upload.onprogress = e => {
+
+let p = Math.round((e.loaded/e.total)*100)
+
+document.getElementById("progress").innerText = p+"%"
+
 }
 
-/* Receive DM */
-socket.on("dm", (data) => {
-document.getElementById("dmMessages").innerHTML += "<div><b>${data.from}:</b> ${data.msg}</div>";
-});
+xhr.onload = () => {
 
-/* File upload (GIF / image) */
-document.getElementById("file").onchange = function () {
-let file = this.files[0];
-let form = new FormData();
-form.append("file", file);
+let url = xhr.responseText
 
-let xhr = new XMLHttpRequest();
-xhr.open("POST", "/upload");
+socket.emit("message",{
+room:room,
+user:username,
+msg:"<img src='"+url+"'>"
+})
 
-xhr.upload.onprogress = function (e) {
-let percent = Math.round((e.loaded / e.total) * 100);
-document.getElementById("progress").innerText = percent + "%";
-};
+}
 
-xhr.onload = function () {
-let res = JSON.parse(xhr.response);
-let url = res.url;
+xhr.send(form)
 
-socket.emit("message", {
-  user: username,
-  msg: `<img src="${url}" width="200">`,
-  room
-});
-
-document.getElementById("progress").innerText = "";
-
-};
-
-xhr.send(form);
-};
+  }
