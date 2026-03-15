53bad5d49e78a3ad525312e5ea8797350e77120e
florawings <epffo.portal@gmail.com>
2026-03-15 21:05:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,79 +1,84 @@
 const express = require("express")
-const http = require("http")
-const { Server } = require("socket.io")
-const multer = require("multer")
-const path = require("path")
-
 const app = express()
-const server = http.createServer(app)
-const io = new Server(server)
+const http = require("http").createServer(app)
+const io = require("socket.io")(http)
 
 app.use(express.static("public"))
 
-/* upload */
-
-const storage = multer.diskStorage({
-destination: "public/uploads",
-filename: (req,file,cb)=>{
-cb(null, Date.now()+path.extname(file.originalname))
-}
-})
-
-const upload = multer({storage})
-
-app.post("/upload", upload.single("file"), (req,res)=>{
-res.json({url:"/uploads/"+req.file.filename})
-})
-
 let users = {}
-let usernames = {}
+let messages = []
 
 io.on("connection",(socket)=>{
 
 socket.on("join",(data)=>{
 
-socket.username = data.username
-socket.room = data.room
-
 socket.join(data.room)
 
-users[socket.id] = data.room
-usernames[socket.id] = data.username
+users[socket.id] = {
+name:data.username,
+room:data.room
+}
 
-io.to(data.room).emit("system", data.username+" joined the room")
+io.to(data.room).emit("system",data.username+" joined "+data.room)
 
-io.emit("online", Object.values(usernames))
+updateUsers(data.room)
 
 })
 
 socket.on("message",(data)=>{
-io.to(data.room).emit("message", data)
-})
 
-socket.on("typing",(data)=>{
-socket.to(data.room).emit("typing", data)
+data.time = Date.now()
+
+messages.push(data)
+
+io.to(data.room).emit("message",data)
+
 })
 
 socket.on("disconnect",()=>{
 
-let name = usernames[socket.id]
-let room = users[socket.id]
+let user = users[socket.id]
+
+if(user){
+
+io.to(user.room).emit("system",user.name+" left")
 
 delete users[socket.id]
-delete usernames[socket.id]
 
-if(name && room){
-io.to(room).emit("system", name+" left the room")
+updateUsers(user.room)
+
 }
 
-io.emit("online", Object.values(usernames))
-
 })
 
 })
 
-const PORT = process.env.PORT || 3000
+function updateUsers(room){
 
-server.listen(PORT,()=>{
-console.log("TulipChat running on "+PORT)
-})
+let list = []
+
+for(let id in users){
+
+if(users[id].room==room){
+
+list.push(users[id].name)
+
+}
+
+}
+
+io.to(room).emit("online",list)
+
+}
+
+/* auto delete 30 min messages */
+
+setInterval(()=>{
+
+let now = Date.now()
+
+messages = messages.filter(m => now-m.time < 1800000)
+
+},60000)
+
+http.listen(3000)
