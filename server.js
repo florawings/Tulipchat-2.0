4c347900477231a2f1bc77a23c673eba7cfafc77
florawings <epffo.portal@gmail.com>
2026-03-15 15:49:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,35 +1,95 @@
 const express = require("express")
 const http = require("http")
-const {Server} = require("socket.io")
-const cors = require("cors")
+const { Server } = require("socket.io")
+const mongoose = require("mongoose")
+const path = require("path")
 
+// database connect
 require("./database")
 
-const authRoutes = require("./routes/auth")
+const User = require("./models/User")
 
 const app = express()
 const server = http.createServer(app)
 
-const io = new Server(server)
+const io = new Server(server,{
+cors:{
+origin:"*"
+}
+})
 
-app.use(cors())
 app.use(express.json())
-
-app.use("/api",authRoutes)
-
 app.use(express.static("public"))
 
-let users = {}
+let onlineUsers = {}
 
+// register API
+app.post("/api/register", async(req,res)=>{
+
+try{
+
+const {username,email,password} = req.body
+
+const user = new User({
+username,
+email,
+password
+})
+
+await user.save()
+
+res.json({status:"ok"})
+
+}catch(err){
+
+res.json({status:"error"})
+
+}
+
+})
+
+// login API
+app.post("/api/login", async(req,res)=>{
+
+const {email,password} = req.body
+
+const user = await User.findOne({email})
+
+if(!user){
+
+return res.json({status:"no user"})
+
+}
+
+if(user.password !== password){
+
+return res.json({status:"wrong password"})
+
+}
+
+res.json({
+status:"ok",
+username:user.username
+})
+
+})
+
+
+// realtime chat
 io.on("connection",(socket)=>{
+
+console.log("user connected:",socket.id)
 
 socket.on("join",(username)=>{
 
-users[socket.id]=username
+onlineUsers[socket.id] = username
 
-io.emit("onlineUsers",Object.values(users))
+io.emit("onlineUsers",Object.values(onlineUsers))
+
+io.emit("system",username + " joined chat")
 
 })
+
 
 socket.on("chat message",(data)=>{
 
@@ -37,14 +97,30 @@ io.emit("chat message",data)
 
 })
 
+
 socket.on("disconnect",()=>{
 
-delete users[socket.id]
+let username = onlineUsers[socket.id]
 
-io.emit("onlineUsers",Object.values(users))
+delete onlineUsers[socket.id]
+
+if(username){
+
+io.emit("system",username + " left chat")
+
+}
+
+io.emit("onlineUsers",Object.values(onlineUsers))
 
 })
 
 })
 
-server.listen(process.env.PORT || 3000)
+
+const PORT = process.env.PORT || 3000
+
+server.listen(PORT,()=>{
+
+console.log("server running on port",PORT)
+
+})
