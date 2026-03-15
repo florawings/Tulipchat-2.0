befbeda827374bdc75cfa24867743d45477c3a44
florawings <epffo.portal@gmail.com>
2026-03-15 20:29:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,33 +1,36 @@
-const express=require("express")
-const http=require("http")
-const {Server}=require("socket.io")
-const multer=require("multer")
-const path=require("path")
+const express = require("express")
+const http = require("http")
+const {Server} = require("socket.io")
+const multer = require("multer")
+const path = require("path")
 
-const app=express()
-const server=http.createServer(app)
-const io=new Server(server)
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server)
 
 app.use(express.static("public"))
 
-/* upload */
+/* upload system */
 
-const storage=multer.diskStorage({
-destination:"public/uploads",
-filename:(req,file,cb)=>{
+const storage = multer.diskStorage({
+destination: "public/uploads",
+filename: (req,file,cb)=>{
 cb(null,Date.now()+path.extname(file.originalname))
 }
 })
 
-const upload=multer({storage})
+const upload = multer({storage})
 
-app.post("/upload",upload.single("file"),(req,res)=>{
+app.post("/upload", upload.single("file"), (req,res)=>{
 res.json({url:"/uploads/"+req.file.filename})
 })
 
-/* users */
+/* memory users */
 
 let users={}
+let usernames={}
+
+/* socket */
 
 io.on("connection",(socket)=>{
 
@@ -38,42 +41,53 @@ socket.room=data.room
 
 socket.join(data.room)
 
-users[socket.id]=data.username
+users[socket.id]=data.room
+usernames[socket.id]=data.username
 
 io.to(data.room).emit("system",data.username+" joined the room")
 
-io.emit("online",Object.values(users))
+io.emit("online",Object.values(usernames))
 
 })
 
-socket.on("chat message",(data)=>{
-
-io.to(data.room).emit("chat message",data)
-
+socket.on("message",(data)=>{
+io.to(data.room).emit("message",data)
 })
 
-socket.on("typing",(name)=>{
+socket.on("typing",(data)=>{
+socket.to(data.room).emit("typing",data)
+})
 
-socket.broadcast.emit("typing",name+" typing...")
+socket.on("dm",(data)=>{
+
+for(let id in usernames){
+if(usernames[id]===data.to){
+io.to(id).emit("dm",data)
+}
+}
 
 })
 
 socket.on("disconnect",()=>{
 
-let name=users[socket.id]
+let name=usernames[socket.id]
+let room=users[socket.id]
 
 delete users[socket.id]
+delete usernames[socket.id]
 
-if(name){
-io.emit("system",name+" left the room")
+if(name && room){
+io.to(room).emit("system",name+" left the room")
 }
 
-io.emit("online",Object.values(users))
+io.emit("online",Object.values(usernames))
 
 })
 
 })
 
-server.listen(3000,()=>{
-console.log("Tulip Chat PRO running")
+const PORT = process.env.PORT || 3000
+
+server.listen(PORT,()=>{
+console.log("Tulip Chat running "+PORT)
 })
