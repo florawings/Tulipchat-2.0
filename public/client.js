0b3b6e114689c7a13d8036a84ad812f438afadc6
florawings <epffo.portal@gmail.com>
2026-03-15 21:20:03 +0530

    Update client.js

--- public/client.js
+++ public/client.js
@@ -2,47 +2,131 @@ const socket = io()
 
 let username = localStorage.getItem("username")
 
-socket.emit("join",username)
+if(!username){
 
-function sendMsg(){
+username = prompt("Enter username")
 
-let input=document.getElementById("msgInput")
-
-socket.emit("chat message",{
-
-user:username,
-text:input.value
-
-})
-
-input.value=""
+localStorage.setItem("username",username)
 
 }
 
-socket.on("chat message",(data)=>{
+let room = "normal"
+let currentDM = null
 
-let div=document.createElement("div")
+socket.emit("join",{username,room})
 
-div.innerText=data.user+": "+data.text
+/* ROOM JOIN */
 
-document.getElementById("messages").appendChild(div)
+function joinRoom(r){
+
+room = r
+
+socket.emit("join",{username,room})
+
+document.getElementById("messages").innerHTML=""
+
+}
+
+/* SEND ROOM MESSAGE */
+
+function sendMessage(){
+
+let text = document.getElementById("text").value
+
+if(!text) return
+
+socket.emit("message",{
+
+user:username,
+msg:text,
+room:room
 
 })
 
-socket.on("onlineUsers",(list)=>{
+document.getElementById("text").value=""
 
-let div=document.getElementById("users")
+}
 
-div.innerHTML=""
+/* RECEIVE ROOM MESSAGE */
+
+socket.on("message",(data)=>{
+
+let box=document.getElementById("messages")
+
+box.innerHTML += `<div><b>${data.user}</b>: ${data.msg}</div>`
+
+box.scrollTop = box.scrollHeight
+
+})
+
+/* SYSTEM MESSAGE */
+
+socket.on("system",(msg)=>{
+
+let box=document.getElementById("messages")
+
+box.innerHTML += `<div style="opacity:.6">${msg}</div>`
+
+})
+
+/* ONLINE USERS */
+
+socket.on("online",(list)=>{
+
+let box=document.getElementById("online")
+
+box.innerHTML=""
 
 list.forEach(u=>{
 
-let d=document.createElement("div")
+let div=document.createElement("div")
 
-d.innerText=u
+div.innerText=u
 
-div.appendChild(d)
+div.onclick=()=>openDM(u)
+
+box.appendChild(div)
 
 })
+
+})
+
+/* OPEN DM */
+
+function openDM(user){
+
+currentDM = user
+
+document.getElementById("dmBox").style.display="block"
+
+document.getElementById("dmUser").innerText="Chat with "+user
+
+}
+
+/* SEND DM */
+
+function sendDM(){
+
+let text=document.getElementById("dmText").value
+
+socket.emit("dm",{
+
+from:username,
+to:currentDM,
+msg:text
+
+})
+
+document.getElementById("dmMessages").innerHTML += `<div><b>Me:</b> ${text}</div>`
+
+document.getElementById("dmText").value=""
+
+}
+
+/* RECEIVE DM */
+
+socket.on("dm",(data)=>{
+
+document.getElementById("dmMessages").innerHTML += `<div><b>${data.from}:</b> ${data.msg}</div>`
 
 })
