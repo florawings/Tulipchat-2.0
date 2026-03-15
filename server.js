98530e4e26f43325c0d0428d5f4fa99df4eb3657
florawings <epffo.portal@gmail.com>
2026-03-15 23:50:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,54 +1,84 @@
 const express = require("express")
-const http = require("http")
-const { Server } = require("socket.io")
-const multer = require("multer")
-
 const app = express()
-const server = http.createServer(app)
-const io = new Server(server)
+const http = require("http").createServer(app)
+const io = require("socket.io")(http)
 
 app.use(express.static("public"))
-app.use("/uploads",express.static("uploads"))
+app.use(express.json())
 
-/* upload */
-
-const storage = multer.diskStorage({
-destination:(req,file,cb)=>cb(null,"uploads/"),
-filename:(req,file,cb)=>cb(null,Date.now()+"_"+file.originalname)
-})
-
-const upload = multer({storage})
-
-app.post("/upload",upload.single("file"),(req,res)=>{
-res.json({url:"/uploads/"+req.file.filename})
-})
-
-/* users */
-
-const users={}
+let users = {}
+let messages = []   // chat history
 
 io.on("connection",(socket)=>{
 
+// JOIN CHAT
 socket.on("join",(data)=>{
-users[socket.id]=data.name
+
+users[socket.id] = data.name
+
+// send old messages
+socket.emit("history",messages)
+
+// broadcast join message
+let joinMsg = {
+user:"System",
+msg:data.name + " joined the chat"
+}
+
+messages.push(joinMsg)
+
+io.emit("message",joinMsg)
+
 io.emit("online",users)
+
 })
 
+// PUBLIC MESSAGE
 socket.on("message",(data)=>{
+
+messages.push(data)
+
+if(messages.length>100){
+messages.shift()
+}
+
 io.emit("message",data)
+
 })
 
+// DM
 socket.on("dm",(data)=>{
+
 io.to(data.to).emit("dm",data)
+
 })
 
+// DISCONNECT
 socket.on("disconnect",()=>{
+
+let name = users[socket.id]
+
 delete users[socket.id]
+
 io.emit("online",users)
-})
+
+if(name){
+
+let leaveMsg={
+user:"System",
+msg:name + " left the chat"
+}
+
+messages.push(leaveMsg)
+
+io.emit("message",leaveMsg)
+
+}
 
 })
 
-server.listen(3000,()=>{
-console.log("Tulip Chat running")
+})
+
+http.listen(3000,()=>{
+console.log("Server running")
 })
