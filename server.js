a3818dff88b0c69a3b50888fd90f4f39816b2f96
florawings <epffo.portal@gmail.com>
2026-03-15 15:40:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,79 +1,47 @@
 const express = require("express")
 const http = require("http")
-const { Server } = require("socket.io")
+const {Server} = require("socket.io")
+const cors = require("cors")
+
+require("./database")
+
+const authRoutes = require("./routes/auth")
 
 const app = express()
 const server = http.createServer(app)
+
 const io = new Server(server)
+
+app.use(cors())
+app.use(express.json())
+
+app.use("/api",authRoutes)
 
 app.use(express.static("public"))
 
 let users = {}
-let blocked = {}
 
 io.on("connection",(socket)=>{
 
 socket.on("join",(username)=>{
 
-users[socket.id] = username
+users[socket.id]=username
 
-io.emit("users",Object.values(users))
-io.emit("onlineCount",Object.keys(users).length)
-
-io.emit("chat message",{
-type:"system",
-text: username + " joined chat"
-})
+io.emit("onlineUsers",Object.values(users))
 
 })
 
 socket.on("chat message",(data)=>{
+
 io.emit("chat message",data)
-})
 
-socket.on("dm",(data)=>{
-
-let target = Object.keys(users).find(
-id => users[id] === data.to
-)
-
-if(target){
-io.to(target).emit("dm",data)
-}
-
-})
-
-socket.on("friend request",(data)=>{
-
-let target = Object.keys(users).find(
-id => users[id] === data.to
-)
-
-if(target){
-io.to(target).emit("friend request",data)
-}
-
-})
-
-socket.on("send gift",(data)=>{
-io.emit("gift",data)
 })
 
 socket.on("disconnect",()=>{
 
-let username = users[socket.id]
-
 delete users[socket.id]
 
-io.emit("users",Object.values(users))
-io.emit("onlineCount",Object.keys(users).length)
-
-if(username){
-io.emit("chat message",{
-type:"system",
-text: username + " left chat"
-})
-}
+io.emit("onlineUsers",Object.values(users))
 
 })
 
