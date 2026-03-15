cfdad68d1310ceb402e3484b67ff1c5ddae91fa1
florawings <epffo.portal@gmail.com>
2026-03-15 16:22:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,72 +2,23 @@ const express = require("express")
 const http = require("http")
 const { Server } = require("socket.io")
 const mongoose = require("mongoose")
+const path = require("path")
 
-require("./database")
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server)
+
+app.use(express.json())
+app.use(express.static(path.join(__dirname,"public")))
+
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>console.log("MongoDB Connected"))
+.catch(err=>console.log(err))
 
 const User = require("./models/User")
 const Message = require("./models/Message")
 
-const app = express()
-const server = http.createServer(app)
-
-const io = new Server(server)
-
-app.use(express.json())
-app.use(express.static("public"))
-
 let onlineUsers = {}
-
-/* ---------- REGISTER ---------- */
-
-app.post("/api/register", async (req,res)=>{
-
-try{
-
-const {username,email,password} = req.body
-
-const user = new User({
-username,
-email,
-password
-})
-
-await user.save()
-
-res.json({status:"ok"})
-
-}catch(err){
-
-res.json({status:"error"})
-
-}
-
-})
-
-/* ---------- LOGIN ---------- */
-
-app.post("/api/login", async (req,res)=>{
-
-const {email,password} = req.body
-
-let user = await User.findOne({email})
-
-if(!user){
-return res.json({status:"no user"})
-}
-
-if(user.password !== password){
-return res.json({status:"wrong password"})
-}
-
-res.json({
-status:"ok",
-username:user.username
-})
-
-})
-
-/* ---------- SOCKET ---------- */
 
 io.on("connection",(socket)=>{
 
@@ -79,11 +30,11 @@ io.emit("onlineUsers",Object.keys(onlineUsers))
 
 })
 
-/* CHAT MESSAGE */
+socket.on("chat message", async(data)=>{
 
-socket.on("chat message", async (data)=>{
+try{
 
-let msg = new Message({
+const msg = new Message({
 username:data.username,
 msg:data.msg
 })
@@ -92,31 +43,11 @@ await msg.save()
 
 io.emit("chat message",data)
 
-})
-
-/* TYPING */
-
-socket.on("typing",(username)=>{
-
-socket.broadcast.emit("typing",username)
-
-})
-
-/* DM */
-
-socket.on("dm",(data)=>{
-
-let target = onlineUsers[data.to]
-
-if(target){
-
-io.to(target).emit("dm",data)
-
+}catch(err){
+console.log(err)
 }
 
 })
-
-/* DISCONNECT */
 
 socket.on("disconnect",()=>{
 
@@ -136,6 +67,46 @@ io.emit("onlineUsers",Object.keys(onlineUsers))
 
 })
 
-server.listen(3000,()=>{
-console.log("server running")
+app.post("/register",async(req,res)=>{
+
+try{
+
+const user = new User(req.body)
+
+await user.save()
+
+res.json({status:"ok"})
+
+}catch(err){
+
+res.json({status:"error"})
+
+}
+
+})
+
+app.post("/login",async(req,res)=>{
+
+const {email,password} = req.body
+
+const user = await User.findOne({email,password})
+
+if(user){
+
+res.json({status:"ok",username:user.username})
+
+}else{
+
+res.json({status:"fail"})
+
+}
+
+})
+
+const PORT = process.env.PORT || 3000
+
+server.listen(PORT,()=>{
+
+console.log("Server running on",PORT)
+
 })
