03901112b6138bea5634adea2188ddc7f0c6f061
florawings <epffo.portal@gmail.com>
2026-03-15 15:12:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,20 @@
 const express = require("express")
 const http = require("http")
 const { Server } = require("socket.io")
+const path = require("path")
 
 const app = express()
 const server = http.createServer(app)
-
-const io = new Server(server,{
-cors:{origin:"*"}
-})
+const io = new Server(server)
 
 app.use(express.static("public"))
+app.use('/uploads', express.static('uploads'))
 
 let users = {}
 
 io.on("connection",(socket)=>{
 
-console.log("Connected:",socket.id)
-
-/* JOIN */
+console.log("connected",socket.id)
 
 socket.on("join",(username)=>{
 
@@ -27,38 +24,28 @@ io.emit("users",Object.values(users))
 
 io.emit("chat message",{
 type:"system",
-text:username+" joined chat"
+text: username + " joined chat"
 })
 
 })
-
-/* MESSAGE */
 
 socket.on("chat message",(data)=>{
-
 io.emit("chat message",data)
-
 })
-
-/* DM */
 
 socket.on("dm",(data)=>{
 
 let target = Object.keys(users).find(
-id => users[id]===data.to
+id => users[id] === data.to
 )
 
 if(target){
-
 io.to(target).emit("dm",data)
-
 }
 
 socket.emit("dm",data)
 
 })
-
-/* DISCONNECT */
 
 socket.on("disconnect",()=>{
 
@@ -69,20 +56,16 @@ delete users[socket.id]
 io.emit("users",Object.values(users))
 
 if(username){
-
 io.emit("chat message",{
 type:"system",
-text:username+" left chat"
+text: username + " left chat"
 })
-
 }
 
 })
 
 })
 
-const PORT = process.env.PORT || 3000
-
-server.listen(PORT,()=>{
-console.log("Server running",PORT)
+server.listen(process.env.PORT || 3000,()=>{
+console.log("server running")
 })
