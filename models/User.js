c81314288ed111e2b0c39b744445fc3085deb241
florawings <epffo.portal@gmail.com>
2026-03-15 16:15:35 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,30 +2,9 @@ const mongoose = require("mongoose")
 
 const userSchema = new mongoose.Schema({
 
-username:{
-type:String,
-required:true
-},
-
-email:{
-type:String,
-required:true
-},
-
-password:{
-type:String,
-required:true
-},
-
-friends:{
-type:[String],
-default:[]
-},
-
-blocked:{
-type:[String],
-default:[]
-}
+username:String,
+email:String,
+password:String
 
 })
 
