09d425be22d071a3c2f69b86b9b8e55115417394
florawings <epffo.portal@gmail.com>
2026-03-15 22:00:09 +0530

    Update client.js

--- public/client.js
+++ public/client.js
@@ -1,22 +1,38 @@
 const socket = io()
 
 let room = "normal"
-let username = localStorage.getItem("user") || prompt("Enter username")
+
+let username = localStorage.getItem("user")
+
+if(!username){
+username = prompt("Enter username")
+localStorage.setItem("user",username)
+}
 
 socket.emit("user",username)
+
+/* default room join */
+
+socket.emit("join","normal")
 
 function joinRoom(r){
 
 room = r
 
+document.getElementById("messages").innerHTML=""
+
 socket.emit("join",room)
 
 }
 
+/* add message */
+
 function addMsg(html){
 
 let div = document.createElement("div")
-div.className = "msg"
+
+div.className="msg"
+
 div.innerHTML = html
 
 document.getElementById("messages").appendChild(div)
@@ -25,6 +41,8 @@ document.getElementById("messages").scrollTop =
 document.getElementById("messages").scrollHeight
 
 }
+
+/* send message */
 
 function sendMsg(){
 
@@ -42,26 +60,29 @@ document.getElementById("text").value=""
 
 }
 
+/* receive message */
+
 socket.on("message",(data)=>{
 
 addMsg("<b>"+data.user+":</b> "+data.msg)
 
 })
 
-/* upload */
+/* upload image / gif */
 
 document.getElementById("file").onchange = function(){
 
 let file = this.files[0]
 
 let form = new FormData()
+
 form.append("file",file)
 
 let xhr = new XMLHttpRequest()
 
 xhr.open("POST","/upload")
 
-xhr.upload.onprogress = e => {
+xhr.upload.onprogress = e =>{
 
 let p = Math.round((e.loaded/e.total)*100)
 
@@ -69,7 +90,7 @@ document.getElementById("progress").innerText = p+"%"
 
 }
 
-xhr.onload = () => {
+xhr.onload = ()=>{
 
 let url = xhr.responseText
 
@@ -83,4 +104,4 @@ msg:"<img src='"+url+"'>"
 
 xhr.send(form)
 
-  }
+}
