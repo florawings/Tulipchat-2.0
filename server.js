32c79c973272ea1426a3443ba0a209a29bf8c279
florawings <epffo.portal@gmail.com>
2026-03-15 20:43:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,13 +15,13 @@ app.use(express.static("public"))
 const storage = multer.diskStorage({
 destination: "public/uploads",
 filename: (req,file,cb)=>{
-cb(null,Date.now()+path.extname(file.originalname))
+cb(null, Date.now()+path.extname(file.originalname))
 }
 })
 
 const upload = multer({storage})
 
-app.post("/upload",upload.single("file"),(req,res)=>{
+app.post("/upload", upload.single("file"), (req,res)=>{
 res.json({url:"/uploads/"+req.file.filename})
 })
 
@@ -40,28 +40,18 @@ socket.join(data.room)
 users[socket.id] = data.room
 usernames[socket.id] = data.username
 
-io.to(data.room).emit("system",data.username+" joined the room")
+io.to(data.room).emit("system", data.username+" joined the room")
 
-io.emit("online",Object.values(usernames))
+io.emit("online", Object.values(usernames))
 
 })
 
 socket.on("message",(data)=>{
-io.to(data.room).emit("message",data)
+io.to(data.room).emit("message", data)
 })
 
 socket.on("typing",(data)=>{
-socket.to(data.room).emit("typing",data)
-})
-
-socket.on("dm",(data)=>{
-
-for(let id in usernames){
-if(usernames[id] === data.to){
-io.to(id).emit("dm",data)
-}
-}
-
+socket.to(data.room).emit("typing", data)
 })
 
 socket.on("disconnect",()=>{
@@ -73,10 +63,10 @@ delete users[socket.id]
 delete usernames[socket.id]
 
 if(name && room){
-io.to(room).emit("system",name+" left the room")
+io.to(room).emit("system", name+" left the room")
 }
 
-io.emit("online",Object.values(usernames))
+io.emit("online", Object.values(usernames))
 
 })
 
