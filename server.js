61ce031871b6e806ca3aa2a9fae49f34f7c4ad39
florawings <epffo.portal@gmail.com>
2026-03-16 00:27:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,63 +1,62 @@
-let users = {}
-let messages = []
-let dms = {}
+const express=require("express")
+const app=express()
+const http=require("http").createServer(app)
+const io=require("socket.io")(http)
+
+const multer=require("multer")
+
+app.use(express.static("public"))
+
+const storage=multer.diskStorage({
+destination:(req,file,cb)=>{cb(null,"public/uploads")},
+filename:(req,file,cb)=>{cb(null,Date.now()+"-"+file.originalname)}
+})
+
+const upload=multer({storage})
+
+app.post("/upload",upload.single("file"),(req,res)=>{
+res.json({url:"/uploads/"+req.file.filename})
+})
+
+let users={}
+let messages=[]
 
 io.on("connection",(socket)=>{
 
 socket.on("join",(data)=>{
 
-users[socket.id] = {
-name:data.name,
-gender:data.gender
-}
+users[socket.id]={name:data.name,gender:data.gender}
 
 io.emit("online",users)
 
-messages.forEach(m=>{
-socket.emit("message",m)
-})
+messages.forEach(m=>socket.emit("message",m))
 
 })
-
-/* PUBLIC MESSAGE */
 
 socket.on("message",(m)=>{
 
-if(m.msg==="/clear"){
-
+if(m.msg && m.msg.trim().toLowerCase()==="/clear"){
 messages=[]
-
 io.emit("clearChat")
-
 return
-
 }
 
 messages.push(m)
 
-if(messages.length>300){
-messages.shift()
-}
+if(messages.length>300){messages.shift()}
 
 io.emit("message",m)
 
 })
 
-/* DM */
+socket.on("disconnect",()=>{
 
-socket.on("dm",(data)=>{
+delete users[socket.id]
 
-let target=data.to
-let msg=data.msg
-
-if(!dms[target]){
-dms[target]=[]
-}
-
-dms[target].push(data)
-
-io.to(target).emit("dm",data)
+io.emit("online",users)
 
 })
 
 })
+
+http.listen(3000)
