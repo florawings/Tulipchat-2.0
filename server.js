74964119c05c82a474433691bf62a55a3604994b
florawings <epffo.portal@gmail.com>
2026-03-15 21:23:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,72 +1,64 @@
-const express = require("express")
-const app = express()
-const http = require("http").createServer(app)
-const io = require("socket.io")(http)
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const multer = require("multer");
+const path = require("path");
 
-app.use(express.static("public"))
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
-let users = {}   // username -> socket id
-let userRoom = {} // socket -> room
+app.use(express.static("public"));
 
-io.on("connection",(socket)=>{
-
-socket.on("join",(data)=>{
-
-users[data.username] = socket.id
-userRoom[socket.id] = data.room
-
-socket.join(data.room)
-
-io.to(data.room).emit("system",data.username+" joined "+data.room)
-
-updateOnline()
-
-})
-
-socket.on("message",(data)=>{
-
-io.to(data.room).emit("message",data)
-
-})
-
-/* DM MESSAGE */
-
-socket.on("dm",(data)=>{
-
-let target = users[data.to]
-
-if(target){
-
-io.to(target).emit("dm",data)
-
+/* Upload setup */
+const storage = multer.diskStorage({
+destination: function (req, file, cb) {
+cb(null, "uploads/");
+},
+filename: function (req, file, cb) {
+cb(null, Date.now() + path.extname(file.originalname));
 }
+});
+const upload = multer({ storage: storage });
 
-})
+app.post("/upload", upload.single("file"), (req, res) => {
+res.json({ url: "/uploads/" + req.file.filename });
+});
 
-socket.on("disconnect",()=>{
+/* Users */
+let users = {};   // username -> socket id
 
-for(let u in users){
+io.on("connection", (socket) => {
 
-if(users[u] === socket.id){
+socket.on("join", (data) => {
+users[data.username] = socket.id;
+socket.join(data.room);
 
-delete users[u]
+io.to(data.room).emit("system", data.username + " joined " + data.room);
+io.emit("online", Object.keys(users));
 
+});
+
+socket.on("message", (data) => {
+io.to(data.room).emit("message", data);
+});
+
+socket.on("dm", (data) => {
+let target = users[data.to];
+if (target) {
+io.to(target).emit("dm", data);
 }
+});
 
+socket.on("disconnect", () => {
+for (let u in users) {
+if (users[u] === socket.id) delete users[u];
 }
+io.emit("online", Object.keys(users));
+});
 
-delete userRoom[socket.id]
+});
 
-updateOnline()
-
-})
-
-})
-
-function updateOnline(){
-
-io.emit("online",Object.keys(users))
-
-}
-
-http.listen(3000)
+server.listen(3000, () => {
+console.log("TulipChat running on 3000");
+});
