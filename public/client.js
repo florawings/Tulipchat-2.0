2a02e5e50caaa330172ff07b23f7286656b6bbaa
florawings <epffo.portal@gmail.com>
2026-03-15 13:05:03 +0530

    Update client.js

--- public/client.js
+++ public/client.js
@@ -1,148 +1,76 @@
-// connect socket
 const socket = io();
 
-// username
 let username = localStorage.getItem("username");
 
-if (!username) {
-  username = prompt("Enter your name");
-  localStorage.setItem("username", username);
+if(!username){
+username = prompt("Enter name");
+localStorage.setItem("username",username);
 }
 
-// join chat
-socket.emit("join", username);
+socket.emit("join",username);
 
-// elements
-const messageBox = document.getElementById("messages");
+const box = document.getElementById("messages");
 const input = document.getElementById("messageInput");
-const onlineBox = document.getElementById("onlineUsers");
 
-// add message to chat
-function addMessage(data) {
+function addMessage(m){
 
-  const div = document.createElement("div");
-  div.className = "message";
+let div=document.createElement("div");
 
-  // system message
-  if (data.type === "system") {
+if(m.type==="system"){
+div.innerHTML="<i>"+m.text+"</i>";
+}
 
-    div.innerHTML = "<i>" + data.text + "</i>";
+else if(m.type==="image"){
+div.innerHTML="<b>"+m.user+":</b><br><img src='"+m.text+"' width='200'>";
+}
 
-  }
+else{
+div.innerHTML="<b>"+m.user+":</b> "+m.text;
+}
 
-  // image or GIF
-  else if (data.type === "image") {
-
-    div.innerHTML =
-      "<b>" + data.user + ":</b><br><img src='" +
-      data.text +
-      "' class='chatImage'>";
-
-  }
-
-  // normal text
-  else {
-
-    div.innerHTML =
-      "<b>" + data.user + ":</b> " + data.text;
-
-  }
-
-  messageBox.appendChild(div);
-
-  messageBox.scrollTop = messageBox.scrollHeight;
+box.appendChild(div);
+box.scrollTop=box.scrollHeight;
 
 }
 
-// receive old messages
-socket.on("oldMessages", (msgs) => {
-
-  messageBox.innerHTML = "";
-
-  msgs.forEach(m => {
-
-    addMessage(m);
-
-  });
-
+socket.on("oldMessages",(msgs)=>{
+msgs.forEach(addMessage);
 });
 
-// receive new message
-socket.on("message", (data) => {
-
-  addMessage(data);
-
+socket.on("message",(m)=>{
+addMessage(m);
 });
 
-// send message
-function sendMessage() {
+function sendMessage(){
 
-  const text = input.value.trim();
+let text=input.value;
 
-  if (!text) return;
+if(!text) return;
 
-  socket.emit("message", {
+socket.emit("message",{
+user:username,
+text:text,
+type:"text"
+});
 
-    user: username,
-    text: text,
-    type: "text"
+input.value="";
+}
 
-  });
+function sendGIF(){
 
-  input.value = "";
+let url=prompt("Paste GIF link");
+
+if(!url) return;
+
+socket.emit("message",{
+user:username,
+text:url,
+type:"image"
+});
 
 }
 
-// enter key send
-input.addEventListener("keypress", function(e){
-
-  if(e.key === "Enter"){
-
-    sendMessage();
-
-  }
-
-});
-
-// online users
-socket.on("onlineUsers", (users) => {
-
-  onlineBox.innerHTML = "";
-
-  users.forEach(u => {
-
-    const div = document.createElement("div");
-
-    div.innerText = u;
-
-    div.onclick = () => openDM(u);
-
-    onlineBox.appendChild(div);
-
-  });
-
-});
-
-// open DM
-function openDM(user){
-
-  const msg = prompt("Send message to " + user);
-
-  if(!msg) return;
-
-  socket.emit("dm", {
-
-    from: username,
-    to: user,
-    text: msg
-
-  });
-
+function logout(){
+localStorage.removeItem("username");
+location.href="login.html";
 }
-
-// receive DM
-socket.on("dm", (data) => {
-
-  alert("DM from " + data.user + ": " + data.text);
-
-});
