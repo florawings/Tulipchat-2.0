59e14a702c439035ca4e96e5e25a664d107f4df8
florawings <epffo.portal@gmail.com>
2026-03-15 16:15:06 +0530

    Update database.js

--- database.js
+++ database.js
@@ -4,11 +4,9 @@ mongoose.connect(process.env.MONGO_URI,{
 useNewUrlParser:true,
 useUnifiedTopology:true
 })
-
 .then(()=>{
 console.log("MongoDB connected")
 })
-
-.catch((err)=>{
+.catch(err=>{
 console.log(err)
 })
