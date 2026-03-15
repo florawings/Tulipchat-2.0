9d3f3e8be3f7ece248d2f4e01905b1d656b7d0ce
florawings <epffo.portal@gmail.com>
2026-03-15 19:47:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,38 +25,35 @@ app.post("/upload",upload.single("file"),(req,res)=>{
 res.json({url:"/uploads/"+req.file.filename})
 })
 
-/* memory db */
+/* memory */
 
-let users={}
-let friends={}
-let privateRoomUsers=["owner"]
+let users = {}
+let privateRoomUsers = ["owner"]
 
 io.on("connection",(socket)=>{
 
 socket.on("join",(data)=>{
 
-socket.username=data.username
-socket.room=data.room
+socket.username = data.username
+socket.room = data.room
 
 if(data.room==="private" && !privateRoomUsers.includes(data.username)){
-socket.emit("denied","Private room access denied")
+socket.emit("system","Private room access denied")
 return
 }
 
 socket.join(data.room)
 
-users[socket.id]=data.username
+users[socket.id] = data.username
 
-io.to(data.room).emit("system",data.username+" joined room")
+io.to(data.room).emit("system",data.username+" joined the room")
 
 io.emit("online",Object.values(users))
 
 })
 
 socket.on("chat message",(data)=>{
-
 io.to(data.room).emit("chat message",data)
-
 })
 
 /* DM */
@@ -75,32 +72,6 @@ io.to(id).emit("dm",data)
 
 })
 
-/* friend request */
-
-socket.on("friend request",(data)=>{
-
-if(!friends[data.to]) friends[data.to]=[]
-
-friends[data.to].push(data.from)
-
-for(let id in users){
-
-if(users[id]===data.to){
-
-io.to(id).emit("friend request",data.from)
-
-}
-
-}
-
-})
-
-socket.on("typing",(user)=>{
-
-socket.broadcast.emit("typing",user)
-
-})
-
 socket.on("disconnect",()=>{
 
 if(users[socket.id]){
@@ -116,7 +87,5 @@ io.emit("online",Object.values(users))
 })
 
 server.listen(3000,()=>{
-
-console.log("Tulip Chat 5.0 running")
-
+console.log("Tulip Chat running")
 })
