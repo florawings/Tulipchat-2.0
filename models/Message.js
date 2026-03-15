6133a6a0d01ec518cc9a5b9abfa8826de6a326a4
florawings <epffo.portal@gmail.com>
2026-03-15 22:50:46 +0530

    Update Message.js

--- models/Message.js
+++ models/Message.js
@@ -1,12 +1,7 @@
-const mongoose = require("mongoose")
-
-const messageSchema = new mongoose.Schema({
-  username: String,
-  message: String,
-  time: {
-    type: Date,
-    default: Date.now
-  }
-})
-
-module.exports = mongoose.model("Message", messageSchema)
+.msg{
+background:#232323;
+padding:12px;
+border-radius:12px;
+max-width:75%;
+box-shadow:0 0 8px rgba(0,0,0,0.3);
+}
