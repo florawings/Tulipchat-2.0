58ebb56a5a1b8598981df16c8e7f7ce431d0c668
florawings <epffo.portal@gmail.com>
2026-03-15 20:39:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const http = require("http")
-const {Server} = require("socket.io")
+const { Server } = require("socket.io")
 const multer = require("multer")
 const path = require("path")
 
@@ -9,16 +9,12 @@ const server = http.createServer(app)
 const io = new Server(server)
 
 app.use(express.static("public"))
-app.use(express.json())
 
-let users={}
-let usernames={}
-
-/* file upload */
+/* upload */
 
 const storage = multer.diskStorage({
-destination:"public/uploads",
-filename:(req,file,cb)=>{
+destination: "public/uploads",
+filename: (req,file,cb)=>{
 cb(null,Date.now()+path.extname(file.originalname))
 }
 })
@@ -29,19 +25,20 @@ app.post("/upload",upload.single("file"),(req,res)=>{
 res.json({url:"/uploads/"+req.file.filename})
 })
 
-/* socket */
+let users = {}
+let usernames = {}
 
 io.on("connection",(socket)=>{
 
 socket.on("join",(data)=>{
 
-socket.username=data.username
-socket.room=data.room
+socket.username = data.username
+socket.room = data.room
 
 socket.join(data.room)
 
-users[socket.id]=data.room
-usernames[socket.id]=data.username
+users[socket.id] = data.room
+usernames[socket.id] = data.username
 
 io.to(data.room).emit("system",data.username+" joined the room")
 
@@ -60,7 +57,7 @@ socket.to(data.room).emit("typing",data)
 socket.on("dm",(data)=>{
 
 for(let id in usernames){
-if(usernames[id]===data.to){
+if(usernames[id] === data.to){
 io.to(id).emit("dm",data)
 }
 }
@@ -69,8 +66,8 @@ io.to(id).emit("dm",data)
 
 socket.on("disconnect",()=>{
 
-let name=usernames[socket.id]
-let room=users[socket.id]
+let name = usernames[socket.id]
+let room = users[socket.id]
 
 delete users[socket.id]
 delete usernames[socket.id]
@@ -85,6 +82,8 @@ io.emit("online",Object.values(usernames))
 
 })
 
-server.listen(3000,()=>{
-console.log("Tulip Chat server running")
+const PORT = process.env.PORT || 3000
+
+server.listen(PORT,()=>{
+console.log("TulipChat running on "+PORT)
 })
