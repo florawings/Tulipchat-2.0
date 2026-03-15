f4ca57da1aca47974e4433a1c80e640404fb2e1f
florawings <epffo.portal@gmail.com>
2026-03-15 20:07:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,53 +1,60 @@
-const express = require("express")
-const http = require("http")
-const { Server } = require("socket.io")
-const multer = require("multer")
-const path = require("path")
+const express=require("express")
+const http=require("http")
+const {Server}=require("socket.io")
+const multer=require("multer")
+const path=require("path")
 
-const app = express()
-const server = http.createServer(app)
-const io = new Server(server)
+const app=express()
+const server=http.createServer(app)
+const io=new Server(server)
 
 app.use(express.static("public"))
 
-/* file upload */
+/* upload */
 
-const storage = multer.diskStorage({
+const storage=multer.diskStorage({
 destination:"public/uploads",
 filename:(req,file,cb)=>{
 cb(null,Date.now()+path.extname(file.originalname))
 }
 })
 
-const upload = multer({storage})
+const upload=multer({storage})
 
 app.post("/upload",upload.single("file"),(req,res)=>{
 res.json({url:"/uploads/"+req.file.filename})
 })
 
-let users = {}
+/* users */
+
+let users={}
 
 io.on("connection",(socket)=>{
 
-socket.on("join",(username)=>{
+socket.on("join",(data)=>{
 
-users[socket.id]=username
+socket.username=data.username
+socket.room=data.room
+
+socket.join(data.room)
+
+users[socket.id]=data.username
+
+io.to(data.room).emit("system",data.username+" joined the room")
 
 io.emit("online",Object.values(users))
-
-io.emit("system",username+" joined the room")
 
 })
 
 socket.on("chat message",(data)=>{
 
-io.emit("chat message",data)
+io.to(data.room).emit("chat message",data)
 
 })
 
 socket.on("typing",(name)=>{
 
-socket.broadcast.emit("typing",name+" is typing...")
+socket.broadcast.emit("typing",name+" typing...")
 
 })
 
@@ -57,16 +64,16 @@ let name=users[socket.id]
 
 delete users[socket.id]
 
-io.emit("online",Object.values(users))
-
 if(name){
 io.emit("system",name+" left the room")
 }
+
+io.emit("online",Object.values(users))
 
 })
 
 })
 
 server.listen(3000,()=>{
-console.log("Tulip Chat running on 3000")
+console.log("Tulip Chat PRO running")
 })
