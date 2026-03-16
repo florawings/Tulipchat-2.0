230ff76484a68e517263710e109fca96d3cf04db
florawings <epffo.portal@gmail.com>
2026-03-16 09:15:44 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,1 +1,32 @@
+const express = require("express")
+const router = express.Router()
 
+let users = []
+
+router.post("/register",(req,res)=>{
+
+const {username,password,email,age,gender} = req.body
+
+users.push({
+username,password,email,age,gender
+})
+
+res.json({msg:"registered"})
+
+})
+
+router.post("/login",(req,res)=>{
+
+const {username,password} = req.body
+
+const user = users.find(u=>u.username===username && u.password===password)
+
+if(!user){
+return res.json({error:"invalid login"})
+}
+
+res.json({msg:"login success",user})
+
+})
+
+module.exports = router
