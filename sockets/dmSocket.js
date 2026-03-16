b98dd6d24911bc7707b7132b4b696dc423eda601
florawings <epffo.portal@gmail.com>
2026-03-16 09:52:24 +0530

    Update dmSocket.js

--- sockets/dmSocket.js
+++ sockets/dmSocket.js
@@ -1,7 +1,26 @@
+let unread={}
+
 module.exports=(io,socket)=>{
 
 socket.on("dm",(data)=>{
-io.to(data.to).emit("dm",data)
+
+const {to,message}=data
+
+if(!unread[to]){
+unread[to]=0
+}
+
+unread[to]++
+
+io.emit("dmMessage",data)
+io.emit("dmBadge",{user:to,count:unread[to]})
+
+})
+
+socket.on("readDM",(user)=>{
+
+unread[user]=0
+
 })
 
 }
