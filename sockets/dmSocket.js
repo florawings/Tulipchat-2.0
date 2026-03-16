52a7f8fca072184cf890b85accce3f9bc062dde3
florawings <epffo.portal@gmail.com>
2026-03-16 09:24:28 +0530

    Update dmSocket.js

--- sockets/dmSocket.js
+++ sockets/dmSocket.js
@@ -1,33 +1,7 @@
-module.exports=(io)=>{
-
-let userSockets={}
-
-io.on("connection",(socket)=>{
-
-socket.on("register",(username)=>{
-userSockets[username]=socket.id
-})
+module.exports=(io,socket)=>{
 
 socket.on("dm",(data)=>{
-
-const target=userSockets[data.to]
-
-if(target){
-io.to(target).emit("dm",data)
-}
-
-})
-
-socket.on("disconnect",()=>{
-
-for(const user in userSockets){
-if(userSockets[user]===socket.id){
-delete userSockets[user]
-}
-}
-
-})
-
+io.to(data.to).emit("dm",data)
 })
 
 }
