1135e40df407ca0a877803a11dc4d0275655ec0a
florawings <epffo.portal@gmail.com>
2026-03-17 00:21:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,59 +1,47 @@
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
-const path = require("path");
+const express = require("express")
+const http = require("http")
+const { Server } = require("socket.io")
+const path = require("path")
 
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server)
 
-app.use(express.json());
+app.use(express.json())
 
-/* static files */
-app.use(express.static(path.join(__dirname, "public")));
+/* PUBLIC FILES */
 
-/* ROUTES IMPORT */
-const authRoutes = require("./routes/auth");
-const adminRoutes = require("./routes/admin");
-const friendsRoutes = require("./routes/friends");
-const reportRoutes = require("./routes/report");
-const shopRoutes = require("./routes/shop");
+app.use(express.static(path.join(__dirname,"public")))
 
-/* ROUTES USE */
-app.use("/api/auth", authRoutes);
-app.use("/api/admin", adminRoutes);
-app.use("/api/friends", friendsRoutes);
-app.use("/api/report", reportRoutes);
-app.use("/api/shop", shopRoutes);
+/* ROOT */
 
-/* HOME PAGE */
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/index.html"));
-});
+app.get("/",(req,res)=>{
+res.sendFile(path.join(__dirname,"public/login.html"))
+})
 
-/* LOGIN PAGE */
-app.get("/login", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/login.html"));
-});
+/* PAGES */
 
-/* REGISTER PAGE */
-app.get("/register", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/register.html"));
-});
+app.get("/login",(req,res)=>{
+res.sendFile(path.join(__dirname,"public/login.html"))
+})
 
-/* CHAT PAGE */
-app.get("/chat", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/chat.html"));
-});
+app.get("/register",(req,res)=>{
+res.sendFile(path.join(__dirname,"public/register.html"))
+})
+
+app.get("/chat",(req,res)=>{
+res.sendFile(path.join(__dirname,"public/chat.html"))
+})
 
 /* SOCKETS */
-require("./sockets/chatSocket")(io);
-require("./sockets/dmSocket")(io);
+
+require("./sockets/chatSocket")(io)
+require("./sockets/dmSocket")(io)
 
 /* START SERVER */
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
-server.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
-});
+server.listen(PORT,()=>{
+console.log("Server running on "+PORT)
+})
