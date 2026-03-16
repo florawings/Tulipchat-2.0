3b5328a492e972e071fbf464571a434579683a64
florawings <epffo.portal@gmail.com>
2026-03-16 09:25:07 +0530

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -1,20 +1,15 @@
-const express = require("express")
-const router = express.Router()
+const express=require("express")
+const router=express.Router()
 
-let banned = []
+let banned=[]
 
 router.post("/ban",(req,res)=>{
-
 banned.push(req.body.username)
-
 res.json({msg:"user banned"})
-
 })
 
 router.get("/banned",(req,res)=>{
-
 res.json(banned)
-
 })
 
-module.exports = router
+module.exports=router
