a77865ceac1136255ab61ca85bfc0e51b5113acf
florawings <epffo.portal@gmail.com>
2026-03-16 13:17:51 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,14 +2,18 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  password: { type: String, required: false }, // Guest ke liye optional
+  email: { type: String },
+  age: { type: Number },
+  gender: { type: String },
   role: { 
     type: String, 
-    enum: ['user', 'mod', 'admin', 'owner'], 
+    enum: ['user', 'admin', 'owner', 'guest'], 
     default: 'user' 
   },
   isBanned: { type: Boolean, default: false },
-  isMuted: { type: Boolean, default: false }
+  isMuted: { type: Boolean, default: false },
+  createdAt: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('User', userSchema);
