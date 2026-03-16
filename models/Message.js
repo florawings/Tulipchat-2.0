a824c4b7aeb6e9deb2a4b2b80b2f92d93491b8ab
florawings <epffo.portal@gmail.com>
2026-03-16 09:23:27 +0530

    Update Message.js

--- models/Message.js
+++ models/Message.js
@@ -1,7 +1,9 @@
-.msg{
-background:#232323;
-padding:12px;
-border-radius:12px;
-max-width:75%;
-box-shadow:0 0 8px rgba(0,0,0,0.3);
-}
+const mongoose=require("mongoose")
+
+const messageSchema=new mongoose.Schema({
+user:String,
+text:String,
+time:Date
+})
+
+module.exports=mongoose.model("Message",messageSchema)
