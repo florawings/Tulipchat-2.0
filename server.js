9d3d21803cf472439cb5bd1aa295174f5a64f4d9
florawings <epffo.portal@gmail.com>
2026-03-16 11:22:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,14 +7,69 @@ const app = express()
 const server = http.createServer(app)
 const io = new Server(server)
 
+app.use(express.json())
 app.use(express.static(path.join(__dirname,"public")))
+
+/* USERS MEMORY */
+
+let users = [
+ { username:"Lord_lucifer", password:"766521", role:"owner" }
+]
+
+/* REGISTER */
+
+app.post("/register",(req,res)=>{
+
+ const {username,password} = req.body
+
+ if(!username || !password){
+  return res.json({error:"Missing fields"})
+ }
+
+ const exist = users.find(u=>u.username===username)
+
+ if(exist){
+  return res.json({error:"User exists"})
+ }
+
+ users.push({
+  username,
+  password,
+  role:"user"
+ })
+
+ res.json({success:true})
+
+})
+
+/* LOGIN */
+
+app.post("/login",(req,res)=>{
+
+ const {username,password}=req.body
+
+ const user = users.find(
+  u=>u.username===username && u.password===password
+ )
+
+ if(!user){
+  return res.json({error:"Invalid login"})
+ }
+
+ res.json({
+  success:true,
+  username:user.username,
+  role:user.role
+ })
+
+})
+
+/* SOCKET CHAT */
 
 let onlineUsers=[]
 let dmUsers={}
 
 io.on("connection",(socket)=>{
-
- console.log("User connected")
 
  socket.on("join",(username)=>{
 
@@ -39,14 +94,6 @@ io.on("connection",(socket)=>{
 
   if(target){
    io.to(target).emit("dm",data)
-  }
-
- })
-
- socket.on("clear",()=>{
-
-  if(socket.username==="Lord_lucifer"){
-   io.emit("clearChat")
   }
 
  })
