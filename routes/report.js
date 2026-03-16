2cc8bbd366ee8d82054d0db33eb3170adc8c68a9
florawings <epffo.portal@gmail.com>
2026-03-16 09:25:43 +0530

    Update report.js

--- routes/report.js
+++ routes/report.js
@@ -1,20 +1,15 @@
-const express = require("express")
-const router = express.Router()
+const express=require("express")
+const router=express.Router()
 
-let reports = []
+let reports=[]
 
 router.post("/create",(req,res)=>{
-
 reports.push(req.body)
-
 res.json({msg:"reported"})
-
 })
 
 router.get("/all",(req,res)=>{
-
 res.json(reports)
-
 })
 
-module.exports = router
+module.exports=router
