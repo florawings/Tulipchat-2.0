69a6c018ec440eb6029f7412a22734ce2a41de48
florawings <epffo.portal@gmail.com>
2026-03-16 09:17:34 +0530

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -1,49 +1,20 @@
-const express=require("express")
-const router=express.Router()
+const express = require("express")
+const router = express.Router()
 
-let bannedUsers=[]
-let mutedUsers=[]
+let banned = []
 
 router.post("/ban",(req,res)=>{
-const {username}=req.body
 
-if(!username){
-return res.json({error:"username required"})
-}
+banned.push(req.body.username)
 
-bannedUsers.push(username)
+res.json({msg:"user banned"})
 
-res.json({msg:username+" banned"})
-})
-
-router.post("/mute",(req,res)=>{
-const {username}=req.body
-
-mutedUsers.push(username)
-
-res.json({msg:username+" muted"})
-})
-
-router.post("/unmute",(req,res)=>{
-const {username}=req.body
-
-mutedUsers=mutedUsers.filter(u=>u!==username)
-
-res.json({msg:username+" unmuted"})
-})
-
-router.post("/kick",(req,res)=>{
-const {username}=req.body
-
-res.json({msg:username+" kicked"})
 })
 
 router.get("/banned",(req,res)=>{
-res.json(bannedUsers)
+
+res.json(banned)
+
 })
 
-router.get("/muted",(req,res)=>{
-res.json(mutedUsers)
-})
-
-module.exports=router
+module.exports = router
