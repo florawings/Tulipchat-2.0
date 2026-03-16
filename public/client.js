fd3d1342057f5cde89be4920835decb41e187ac3
florawings <epffo.portal@gmail.com>
2026-03-16 09:17:55 +0530

    Update client.js

--- public/client.js
+++ public/client.js
@@ -1,98 +1,18 @@
-const socket=io()
+const socket = io()
 
-let username=localStorage.getItem("username")
-let gender=localStorage.getItem("gender")
+function sendMessage(){
 
-if(!username){
-location.href="/login.html"
-}
+const msg = document.getElementById("msg").value
 
-socket.emit("join",{user:username,gender})
-
-let currentDM=null
-
-/* MENU */
-
-document.getElementById("menu").onclick=()=>{
-const s=document.getElementById("sidebar")
-s.classList.toggle("open")
-}
-
-/* PUBLIC MESSAGE */
-
-function send(){
-
-let text=document.getElementById("text").value
-if(!text) return
-
-if(currentDM){
-
-socket.emit("dmMessage",{room:currentDM,user:username,msg:text})
-
-}else{
-
-socket.emit("publicMessage",{user:username,msg:text})
+socket.emit("chat message",msg)
 
 }
 
-document.getElementById("text").value=""
-}
+socket.on("chat message",(msg)=>{
 
-/* RECEIVE PUBLIC */
+const div = document.createElement("div")
+div.innerText = msg
 
-socket.on("publicMessage",(data)=>{
-
-if(currentDM) return
-
-let div=document.createElement("div")
-div.className="msg "+(data.user===username?"me":"other")
-div.innerText=data.user+": "+data.msg
-
-document.getElementById("messages").appendChild(div)
-
-})
-
-/* ONLINE USERS */
-
-socket.on("onlineUsers",(list)=>{
-
-let box=document.getElementById("users")
-box.innerHTML=""
-
-for(let id in list){
-
-let u=document.createElement("div")
-u.className="user"
-u.innerText=list[id].name+" ("+list[id].gender+")"
-
-u.onclick=()=>{
-socket.emit("startDM",{to:id,from:username})
-}
-
-box.appendChild(u)
-
-}
-
-})
-
-/* OPEN DM */
-
-socket.on("openDM",(data)=>{
-
-currentDM=data.room
-document.getElementById("title").innerText="DM with "+data.from
-document.getElementById("messages").innerHTML=""
-
-})
-
-/* RECEIVE DM */
-
-socket.on("dmMessage",(data)=>{
-
-let div=document.createElement("div")
-div.className="msg "+(data.user===username?"me":"other")
-div.innerText=data.user+": "+data.msg
-
-document.getElementById("messages").appendChild(div)
+document.getElementById("chat").appendChild(div)
 
 })
