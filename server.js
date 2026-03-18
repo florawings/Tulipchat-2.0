7071a3f660581a27633ed1e5b50fb1104df16d24
florawings <epffo.portal@gmail.com>
2026-03-18 08:36:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,19 +7,15 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// IMPORTANT: static folder
 app.use(express.static(path.join(__dirname, "public")));
 
-// FIX: default route (warna "Cannot GET /" aata hai)
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
-// SIMPLE SOCKET (no crash)
 let users = [];
 
 io.on("connection", (socket) => {
-  console.log("User connected:", socket.id);
 
   socket.on("join", (username) => {
     socket.username = username;
@@ -35,10 +31,11 @@ io.on("connection", (socket) => {
     users = users.filter(u => u !== socket.username);
     io.emit("onlineUsers", users);
   });
+
 });
 
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
-  console.log("Server running on port", PORT);
+  console.log("Server running on port " + PORT);
 });
