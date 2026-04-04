99cfd0c059fef3bd6629456c4ffe6baec10a649d
florawings <epffo.portal@gmail.com>
2026-04-04 23:47:31 +0530

    Update chatSocket.js

--- sockets/chatSocket.js
+++ sockets/chatSocket.js
@@ -1,16 +1,31 @@
-// Backend Logic (Node.js)
-io.on('connection', (socket) => {
-    
-    // 1. Global Message (Sabko dikhega)
-    socket.on('send-global-msg', (data) => {
-        io.emit('receive-global-msg', data); 
-    });
+module.exports = (io) => {
+    io.on('connection', (socket) => {
+        console.log('New User Connected: ' + socket.id);
 
-    // 2. Private Message (Sirf recipient ko)
-    socket.on('send-private-msg', ({ toUserId, message }) => {
-        socket.to(toUserId).emit('receive-private-msg', {
-            from: socket.id,
-            message: message
+        // Join Global Room
+        socket.join('global_room');
+
+        // 1. Global Message Logic
+        socket.on('send-global-msg', (data) => {
+            io.to('global_room').emit('receive-global-msg', {
+                user: data.user,
+                text: data.text,
+                time: new Date().toLocaleTimeString()
+            });
+        });
+
+        // 2. Private DM Logic
+        socket.on('send-private-msg', ({ toUserId, message, fromUser }) => {
+            // Sirf us specific user ko bhejna
+            socket.to(toUserId).emit('receive-private-msg', {
+                from: fromUser,
+                text: message,
+                senderId: socket.id
+            });
+        });
+
+        socket.on('disconnect', () => {
+            console.log('User Disconnected');
         });
     });
-});
+};
