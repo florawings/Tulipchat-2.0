4d54c5372c38ae28325724942dd3211dde848e8e
florawings <epffo.portal@gmail.com>
2026-04-05 00:03:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,28 +6,20 @@ const path = require('path');
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server, {
-    maxHttpBufferSize: 1e7 // 10MB limit photos ke liye
+    maxHttpBufferSize: 1e7 // 10MB (Photos/GIFs ke liye)
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-let onlineUsers = ["Ravindra", "Guest_User", "Admin"]; // Testing ke liye
+let onlineUsers = ["Ravindra", "Admin", "User_01"];
 
 io.on('connection', (socket) => {
-    console.log('User Connected');
-    
-    // Initial data
     socket.emit('update-users', onlineUsers);
 
     socket.on('chat-msg', (data) => {
-        // Sabko message bhej dena (including photos)
         io.emit('new-msg', data);
-    });
-
-    socket.on('disconnect', () => {
-        console.log('User Disconnected');
     });
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Server on ${PORT}`));
+server.listen(PORT, () => console.log(`VibeChat Live on ${PORT}`));
